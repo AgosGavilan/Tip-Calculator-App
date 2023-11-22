2da891645a96c05bfa4a14adc84d7be2e3f15ceb
Agostina Gavilan <agosgavilan3@gmail.com>
2023-11-22 00:06:42 -0300

    prueba

--- src/Calculadora.jsx
+++ src/Calculadora.jsx
@@ -1,15 +1,27 @@
 import React, { useState } from "react";
 
-export const Calculadora = () => {
-    const [bills, setBills] = useState(0) //al principio el dinero va a estar en 0 porque aun no sabemos cuanto gastamos
-    const [props, setProps] = useState([5, 10, 15, 25, 500]) // en un array coloco el porcentaje que viene por default
-    const [personas, setPersonas] = useState(0) //seteamos las personas en 0 al principio
+const Calculadora = () => {
+    //const [bills, setBills] = useState(0) //al principio el dinero va a estar en 0 porque aun no sabemos cuanto gastamos
+    //const [props, setProps] = useState([5, 10, 15, 25, 50]) // en un array coloco el porcentaje que viene por default
+    //const [personas, setPersonas] = useState(0) //seteamos las personas en 0 al principio
+    const [datos, setDatos] = useState({
+        bills: 0,
+        tips: [5, 10, 15, 25, 50],
+        people: 0
+    })
 
     return (
         <div>
             <div className="izquierda">
                 <form>
                     <label>Bills: </label>
+                        <input 
+                        required
+                        type="text"
+                        name='bills'
+                        value={datos.bills}
+                        placeholder="$"
+                       />
                 </form>
             </div>
 
@@ -22,3 +34,5 @@ export const Calculadora = () => {
         </div>
     )
 }
+
+export default Calculadora
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,12 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from './App.jsx'
 
-ReactDOM.render(
-    <>
-        <BrowserRouter>
-            <React.StrictMode>
-                <App />
-            </React.StrictMode>
-        </BrowserRouter>
-    </>,
+
+const root = ReactDOM.createRoot(
     document.getElementById("root")
+);
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 )
